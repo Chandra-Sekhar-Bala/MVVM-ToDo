0978422926384acc9703f4842b97be8de7047f62
Chandra Sekhar <sekhar.chandra.5832@gmail.com>
2022-08-12 23:57:08 +0530

    Add databse save working

--- app/src/main/java/com/sekhar/todo/app/addEditTodo/AddEditTodo.kt
+++ app/src/main/java/com/sekhar/todo/app/addEditTodo/AddEditTodo.kt
@@ -10,9 +10,18 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.sekhar.todo.app.R
 import com.sekhar.todo.app.databinding.FragmentAddTodoBinding
+import com.sekhar.todo.app.repo.db.DAO
+import com.sekhar.todo.app.repo.db.DB
+import com.sekhar.todo.app.repo.db.TodoModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class AddEditTodo : Fragment() {
     private lateinit var bind : FragmentAddTodoBinding
+    private lateinit var dao : DAO
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -20,9 +29,19 @@ class AddEditTodo : Fragment() {
         // Inflate the layout for this fragment
         bind = DataBindingUtil.inflate( inflater, R.layout.fragment_add_todo, container, false)
 
+        initialize()
+
+        return bind.root
+    }
+
+    private fun initialize() {
+
         // Setting Title
         (activity as AppCompatActivity).supportActionBar!!.title = "Add new Todo"
-        return bind.root
+
+        val application = requireNotNull(this.activity).application
+        dao = DB.getInstance(application).todoDAO
+
     }
 
     override fun onResume() {
@@ -30,12 +49,28 @@ class AddEditTodo : Fragment() {
 
         bind.saveTodo.setOnClickListener{
             if(validate()){
-                // TODO : save/ update data to room
-                Toast.makeText(requireContext(), "Saved to DB", Toast.LENGTH_SHORT).show()
+
+                saveData(bind.editTextTodo.text.toString().trim())
             }
         }
 
     }
+
+    private fun saveData(item : String){
+        val data = TodoModel(subject = item)
+
+        GlobalScope.launch(Dispatchers.IO){
+
+            dao.insert(data)
+
+            withContext(Dispatchers.Main){
+                Toast.makeText(requireContext(), "Todo Saved", Toast.LENGTH_SHORT).show()
+                bind.editTextTodo.text?.clear()
+            }
+
+        }
+    }
+
 
     private fun validate(): Boolean {
         var flag = true
