7ae3d8c53c3d93f93c0fa2f31fb6239d3fc6ef4a
Chandra Sekhar <sekhar.chandra.5832@gmail.com>
2022-08-14 02:51:43 +0530

    [Feature] Add motivation quote activity

---
+++ app/src/main/java/com/sekhar/todo/app/motivation/Motivation.kt
@@ -0,0 +1,12 @@
+package com.sekhar.todo.app.motivation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.sekhar.todo.app.R
+
+class Motivation : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_motivation)
+    }
+}
