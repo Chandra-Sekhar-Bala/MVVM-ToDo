3bab034b9e4abb45cf2456c9ab6a5496fd084297
Chandra Sekhar <sekhar.chandra.5832@gmail.com>
2022-08-16 02:52:03 +0530

    Add Coroutine supported Retrofit service method

--- app/src/main/java/com/sekhar/todo/app/repo/network/ApiService.kt
+++ app/src/main/java/com/sekhar/todo/app/repo/network/ApiService.kt
@@ -2,7 +2,6 @@ package com.sekhar.todo.app.repo.network
 
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
@@ -23,7 +22,7 @@ private val retrofit = Retrofit.Builder()
 
 interface ApiService {
     @GET("quotes")
-    fun getMotivation() : Call<List<Motivation>>
+    suspend fun getMotivation() : List<Motivation>
 }
 
 /**
